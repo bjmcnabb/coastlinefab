70e6cf412973c5a25f51eab167e76bfb9e2446ca
Brandon McNabb <68400556+bjmcnabb@users.noreply.github.com>
2023-08-06 13:45:30 -0700

    update files

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicord coastlinefab.wsgi --log-file -
-gunicorn main:app
--- coastlinefab/settings.py
+++ coastlinefab/settings.py
@@ -33,7 +33,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost','coastlinefabricators.ca','https://coastlinefabricatorsca-production.up.railway.app']
