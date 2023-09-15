fc9c4e0f0f8fa12d2785c34da497b46f6a1d6a97
Brandon McNabb <68400556+bjmcnabb@users.noreply.github.com>
2023-09-15 09:24:43 -0700

    update files

--- coastlinefab/settings.py
+++ coastlinefab/settings.py
@@ -229,6 +229,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 # STORAGES = {"staticfiles": {"BACKEND": "storages.backends.s3boto3.S3StaticStorage"}}
 
 if os.getcwd() == '/app':
+    CSRF_TRUSTED_ORIGINS = ["https://coastlinefabricators.ca", "https://www.coastlinefabricators.ca"]
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARED_PROTO', 'https')
     SECURE_SSL_REDIRECT = True
     DEBUG = False
