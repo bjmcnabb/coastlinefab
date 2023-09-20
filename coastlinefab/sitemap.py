fbf87a308d349bfd2efdf7c244b67ed7aa2a32ac
Brandon McNabb <68400556+bjmcnabb@users.noreply.github.com>
2023-09-20 08:50:39 -0700

    update sitemap

--- coastlinefab/sitemap.py
+++ coastlinefab/sitemap.py
@@ -3,6 +3,7 @@ from django.contrib.sitemaps import Sitemap
 from app.models import Album
 
 class coastlinefabsitemap(Sitemap):
+    changefreq = 'weekly'
     priority = 0.5
     protocol = 'https'
 
